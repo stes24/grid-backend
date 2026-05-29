22da763979ef586ac813161b59853eb55dde4e91
stes24 <onafets24@gmail.com>
2026-05-29 22:43:15 +0200

    Piccolo fix dizionari

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -43,17 +44,14 @@ def get_pixels():
     conn = get_connection()
 
     # Oggetto per fare operazioni sul db - esegui SQL e leggi risultati
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=RealDictCursor) # Chiedi di restituire dizionari
     cur.execute("SELECT * FROM pixels")
-    db_rows = cur.fetchall() # Leggi tutte le tuple
-    col_names = [desc[0] for desc in cur.description] # Nomi delle colonne
+    pixels = cur.fetchall() # Leggi tutte le tuple -> Lista di dizionari
 
     # Chiudi connessioni
     cur.close()
     conn.close()
 
-    # Converti le tuple in dizionari con chiavi nominate, poi restituisci come JSON
-    pixels = [dict(zip(col_names, r)) for r in db_rows] # zip crea coppie chiave-valore, dict crea dizionario da esse
     logging.debug("GET /pixels - Invio tutti i pixel")
     return jsonify(pixels)
 
@@ -63,19 +61,17 @@ def get_single_pixel(row, col):
     logging.debug(f"GET /pixels/{row},{col} - Ricevuta chiamata")
 
     conn = get_connection()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute("SELECT * FROM pixels WHERE pixel_row = %s AND pixel_col = %s", (row, col))
-    db_row = cur.fetchone()
-    col_names = [desc[0] for desc in cur.description]
+    pixel = cur.fetchone()
 
     cur.close()
     conn.close()
 
-    if db_row is None:
+    if pixel is None:
         logging.debug(f"GET /pixels/{row},{col} - Pixel non trovato")
         return jsonify({"errore": "Pixel non trovato"}), 404
 
-    pixel = dict(zip(col_names, db_row))
     logging.debug(f"GET /pixels/{row},{col} - Invio il pixel: {pixel}")
     return jsonify(pixel)
 
@@ -113,6 +109,7 @@ def connect_handler():
 def disconnect_handler():
     logging.debug(f"ID {request.sid} - Client disconnesso")
 
+# Aggiorna il colore del pixel con riga e colonna specificati
 @socketio.on("update_pixel")
 def update_pixel_handler(data):
     client_id = request.sid
